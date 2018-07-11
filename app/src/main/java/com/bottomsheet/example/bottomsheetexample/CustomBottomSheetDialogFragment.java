812ebe3afff98ec725235cb9ad450912bbdce3ed
santojor <jorgemiguel.santos@adidas.com>
2018-07-11 21:49:48 +0200

    Initial commit

---
+++ app/src/main/java/com/bottomsheet/example/bottomsheetexample/CustomBottomSheetDialogFragment.java
@@ -0,0 +1,31 @@
+package com.bottomsheet.example.bottomsheetexample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dialog_custom, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view2) {
+                view.findViewById(R.id.view).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
